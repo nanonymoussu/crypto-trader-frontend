2cd81f6b92be6cd481ac1db2b2650a2ad024e1c0
nanonymoussu <nanon2546@yahoo.com>
2025-07-25 06:30:24 +0700

    fix: client-side routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { Toaster } from './components/ui/toaster'
@@ -16,11 +16,11 @@ const App = () => (
         <div className='min-h-screen bg-background'>
           <Toaster />
           <Sonner />
-          <HashRouter>
+          <BrowserRouter>
             <Routes>
               <Route path='/' element={<Home />} />
             </Routes>
-          </HashRouter>
+          </BrowserRouter>
         </div>
       </TooltipProvider>
     </QueryClientProvider>
